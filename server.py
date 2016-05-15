488099e3e681a863720f01c7820f57aaa1eb3695
Augustine Uzokwe <augustine@FRE-OSX-099.local>
2016-05-15 14:35:05 +0200

    defined new port

--- server.py
+++ server.py
@@ -1,4 +1,8 @@
 from flask import Flask
+import os
+
+port = os.environ.get("PORT", 1337)
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,4 +14,4 @@ def hello():
     return "World"
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0",port=port)
